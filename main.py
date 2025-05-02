18b51642250a0de656e52296df9a49cdf3aca072
spetit <sebastien.petit@lne.com>
2025-05-02 22:47:41 +0200

    wip

--- main.py
+++ main.py
@@ -14,13 +14,12 @@ assert gp.num._gpmp_backend_ == "torch", "{} is used, please install Torch.".for
 
 
 path = sys.argv[1]
+n_runs = int(sys.argv[2])
 
 
 # Options
 n_grid = 1000
 n_particles = 150
-
-n_runs = 10
 
 num_new = 1
 
